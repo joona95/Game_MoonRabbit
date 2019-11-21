3604226379fb8fb6152a3a52b8d5202a25fe1027
ryng1034 <ryng1034@gmail.com>
2019-11-21 20:23:22 +0900

    reflect change

---
+++ Game_MoonRabbit/Assets/Scripts/Reflect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflect : MonoBehaviour
+{
+    public Vector3 normalized;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector3 colVec = col.contacts[0].point; //충돌 지점
+        Vector3 currnentVec = this.gameObject.transform.position; //현재 지점
+        Vector3 incomingVec = colVec - currnentVec; //입사 벡터
+        Vector3 normalVec = col.contacts[0].normal; //노말 벡터
+        Vector3 reflectVec = Vector3.Reflect(incomingVec, normalVec); //반사 벡터
+        normalized = reflectVec.normalized; //반사 벡터 정규화
+        GetComponent<Rigidbody2D>().velocity = normalized*4f; //반사
+    }
+}
