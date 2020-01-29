fe309e8a349639e65e405dfa49f97fcd17bd740b
ryng1034 <ryng1034@gmail.com>
2020-01-29 18:03:20 +0900

    200129 stageoption and art

--- Game_MoonRabbit/Assets/Scripts/ChangeScene.cs
+++ Game_MoonRabbit/Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
-
+    public GameObject StageOption;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +26,10 @@ public class ChangeScene : MonoBehaviour
         
     }
 
+    public void StageStartButton()
+    {
+        
+    }
     //back버튼
     public void BackToMapButton()
     {
@@ -36,122 +40,141 @@ public class ChangeScene : MonoBehaviour
     //stage이동 버튼
     public void Stage1_Button()
     {
-        
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 1;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage2_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 2;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage3_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 3;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage4_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 4;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage5_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 5;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage6_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 6;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage7_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 7;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage8_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 8;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage9_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 9;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage10_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 10;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage11_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 11;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage12_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 12;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage13_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 13;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage14_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 14;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage15_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 15;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage16_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 16;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage17_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 17;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage18_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 18;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage19_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 19;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 
     public void Stage20_Button()
     {
+        StageOption.gameObject.SetActive(true);
         Manager.current_stage = 20;
-        SceneManager.LoadScene("Stage");
+        //SceneManager.LoadScene("Stage");
     }
 }
---
+++ Game_MoonRabbit/Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int ChType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChType = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Ch0()
+    {
+        ChType = 0;
+    }
+
+    public void Ch1()
+    {
+        ChType = 1;
+    }
+
+    public void Ch2()
+    {
+        ChType = 2;
+    }
+
+    public void Ch3()
+    {
+        ChType = 3;
+    }
+}
