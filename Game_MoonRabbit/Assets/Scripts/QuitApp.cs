ac838c4731b02b5c5c2b1728da8a818581499341
ryng1034 <ryng1034@gmail.com>
2020-02-27 15:17:24 +0900

    200227 quitapp and build

---
+++ Game_MoonRabbit/Assets/Scripts/QuitApp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+    int ClickCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClickCount++;
+            if (!IsInvoking("DoubleClick"))
+                Invoke("DoubleClick", 1.0f);
+
+        }
+        else if (ClickCount == 2)
+        {
+            CancelInvoke("DoubleClick");
+            Application.Quit();
+        }
+    }
+
+    void DoubleClick()
+    {
+        ClickCount = 0;
+    }
+}
